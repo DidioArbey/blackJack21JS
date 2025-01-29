548b61903e73671d10ca4e9f2352960b629dd3d5
DidioArbey <didioarbey@gmail.com>
2025-01-28 21:56:28 -0500

    turno ordenador

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -5,36 +5,40 @@
  * 2D = Two of Dimonds (Diamantes)
  */
 
-let deck            = [];
-const tipos         = ['C','D','H','S'];
-const especiales    = ['J','Q','K','A'];
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['J', 'Q', 'K', 'A'];
 
 let puntosJugador = 0;
 let puntosOrdenador = 0;
 
 // /referencias HTMl
 const btnPedir = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
+const btnNuevo = document.querySelector('#btnNuevo');
 
-const divCartasJugador = document.querySelector('#jugador-cartas')
+const divCartasJugador      = document.querySelector('#jugador-cartas')
+const divCartasOrdenador    = document.querySelector('#ordenador-cartas')
+// ordenador-cartas
 const puntosHTML = document.querySelectorAll('small');
 
 
 //Esta funcion crea una nueva baraja
-const crearDeck = ()=>{
-    for(let i = 2 ; i <= 10; i++){
-        for( let tipo of tipos){
-            deck.push(i+tipo);
+const crearDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+        for (let tipo of tipos) {
+            deck.push(i + tipo);
         }
     }
 
-    for(let tipo of tipos){
-        for (let esp of especiales){
-            deck.push( esp + tipo);
+    for (let tipo of tipos) {
+        for (let esp of especiales) {
+            deck.push(esp + tipo);
         }
     }
 
     // funcion de undersore para barajar o entregar el arreglo aleatorio
-    deck= _.shuffle(deck);
+    deck = _.shuffle(deck);
     // console.log(deck);
     return deck;
 }
@@ -42,8 +46,8 @@ const crearDeck = ()=>{
 crearDeck();
 
 //esta funcion me permite tomar una carta
-const pedirCarta = ()=>{
-    if(deck.length === 0){
+const pedirCarta = () => {
+    if (deck.length === 0) {
         throw 'No hay más cartas en el Deck'
     }
     const carta = deck.pop();
@@ -54,38 +58,64 @@ const pedirCarta = ()=>{
 pedirCarta();
 
 //Funcion dar valor a la carta
-const valorCarta = (carta)=>{
-    const valor = carta.substring(0,carta.length -1);
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
     return (isNaN(valor)) ?
-            (valor === 'A') ? 11: 10
-            : valor * 1;
+        (valor === 'A') ? 11 : 10
+        : valor * 1;
 }
 
 // const valor = valorCarta(pedirCarta());
 //Turno Ordenador
 
+const turnoOrdenador = (puntosMinimos) => {
+    do {
+        const carta = pedirCarta();
+        puntosOrdenador = puntosOrdenador + valorCarta(carta);
+        puntosHTML[1].innerText = puntosOrdenador;
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${carta}.png`;
+        imgCarta.classList.add('carta');
+        divCartasOrdenador.append(imgCarta);
+        if( puntosMinimos > 21){
+            break;
+        }
 
+    }while((puntosOrdenador < puntosMinimos) && (puntosMinimos <= 21));
+};
 
 //Eventos
-btnPedir.addEventListener('click',() => {
+btnPedir.addEventListener('click', () => {
     const carta = pedirCarta();
 
-    puntosJugador = puntosJugador + valorCarta( carta );
+    puntosJugador = puntosJugador + valorCarta(carta);
     puntosHTML[0].innerText = puntosJugador;
 
     const imgCarta = document.createElement('img');
     imgCarta.src = `assets/cartas/${carta}.png`;
     imgCarta.classList.add('carta');
-    divCartasJugador.append( imgCarta);
+    divCartasJugador.append(imgCarta);
 
-    if (puntosJugador > 21){
+    if (puntosJugador > 21) {
         console.warn('Lo, siento perdiste');
         btnPedir.disabled = true;
-    } else if (puntosJugador === 21){
+        btnDetener.disabled = true;
+        turnoOrdenador(puntosJugador);
+    } else if (puntosJugador === 21) {
         console.warn('21, Genial');
         btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoOrdenador(puntosJugador);
     }
 
-})
+});
 
+// btnDetener
 
+btnDetener.addEventListener('click', () =>{
+   
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoOrdenador(puntosJugador);
+
+});
